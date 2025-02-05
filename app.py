d1bff8a4bfbfbb52624d7384a61fcfd847ec905a
CHETAN MISHRA <mishracchetan619@gmail.com>
2025-02-05 13:32:19 +0530

    updtaed utils exception and logger files

---
+++ app.py
@@ -0,0 +1,9 @@
+import sys,os
+# from wasteDetection.pipeline.training_pipeline import TrainPipeline
+# from wasteDetection.utils.main_utils import decodeImage, encodeImageIntoBase64
+# from flask import Flask, request, jsonify, render_template,Response
+# from flask_cors import CORS, cross_origin
+# from wasteDetection.constant.application import APP_HOST, APP_PORT
+from wasteDetection.logger import logging
+from wasteDetection.exception import AppException
+
