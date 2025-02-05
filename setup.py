01fed2040f9236bb87cbece906f0299ae64f5e55
CHETAN MISHRA <mishracchetan619@gmail.com>
2025-02-05 10:11:27 +0530

    req.txt updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Chetan Mishra',
+    author_email= 'mishracchetan619@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
