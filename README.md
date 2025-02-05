e3b622eef77a1cfd8a6c1e727849231991574d81
CHETAN MISHRA <mishracchetan619@gmail.com>
2025-02-05 16:04:30 +0530

    added training validation model trainer file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Waste-Detection-using-YoloV5
+
+## Workflows
+1. constants
+2. entity
+3. components
+4. pipelines
+5. app.py
+
+#### yolov5 link
+1. git clone https://github.com/ultralytics/yolov5.git
--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import sys,os
-# from wasteDetection.pipeline.training_pipeline import TrainPipeline
-# from wasteDetection.utils.main_utils import decodeImage, encodeImageIntoBase64
-# from flask import Flask, request, jsonify, render_template,Response
-# from flask_cors import CORS, cross_origin
-# from wasteDetection.constant.application import APP_HOST, APP_PORT
-from wasteDetection.logger import logging
-from wasteDetection.exception import AppException
+from wasteDetection.pipeline.training_pipeline import TrainPipeline
+from wasteDetection.utils.main_utils import decodeImage, encodeImageIntoBase64
+from flask import Flask, request, jsonify, render_template,Response
+from flask_cors import CORS, cross_origin
+from wasteDetection.constant.application import APP_HOST, APP_PORT
 
+# from wasteDetection.logger import logging
+# from wasteDetection.exception import AppException
+
+obj = TrainPipeline()
+obj.run_pipeline()
